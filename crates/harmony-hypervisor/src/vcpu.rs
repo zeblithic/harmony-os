e4b18a5f7d900999d71902870b95cdf9c3c43e66
Jake Englund <zeblith@gmail.com>
2026-03-27 09:32:59 -0700

    merge: bring in virtio-net and other main changes

--- crates/harmony-hypervisor/src/vcpu.rs
+++ crates/harmony-hypervisor/src/vcpu.rs
@@ -2,6 +2,7 @@
 //! vCPU context and VM state.
 
 use crate::stage2::Stage2PageTable;
+use crate::uart::VirtualUart;
 use crate::virtio_net::VirtioNetDevice;
 use crate::vmid::VmId;
 
@@ -38,6 +39,7 @@ pub struct Vm {
     pub vcpu: VCpuContext,
     pub stage2: Stage2PageTable,
     pub state: VmState,
+    pub uart: VirtualUart,
     pub virtio_net: VirtioNetDevice,
 }
 
