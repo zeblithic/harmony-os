b33361022680bef0e4e4c3e6fa3639e21f504b73
Jake Englund <zeblith@gmail.com>
2026-04-02 12:10:04 -0700

    feat(netstack): add UdpHandle type and UdpProvider trait

--- crates/harmony-netstack/src/lib.rs
+++ crates/harmony-netstack/src/lib.rs
@@ -9,8 +9,10 @@ pub mod dhcp;
 pub mod peers;
 pub mod stack;
 pub mod tcp;
+pub mod udp;
 
 pub use builder::NetStackBuilder;
 pub use smoltcp;
 pub use stack::NetStack;
 pub use tcp::{NetError, TcpHandle, TcpProvider, TcpSocketState};
+pub use udp::{UdpHandle, UdpProvider};
